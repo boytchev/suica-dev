47bc7466f3fff340e7c925f0b096e7f68bd99c40
boytchev <70705048+boytchev@users.noreply.github.com>
2025-07-31 11:15:22 +0300

    simplified the headers of all examples

--- src/suica.js
+++ src/suica.js
@@ -21,4 +21,8 @@ window.THREE = THREE;
 // based on slightly modified https://github.com/manthrax/THREE-CSGMesh
 //
 // check device.js from suica 2 and the device-orientation example
+//
+// check example randomin-cuboid.html -- maybe one randomin should be randomon?
+//
+// one 3d text examples says suica 2.0 -- make it 3.0
 
